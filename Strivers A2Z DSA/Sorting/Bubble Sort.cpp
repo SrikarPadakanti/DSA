b2b101936652fc995f2bffcdc41a62e7ee22d940
Srikar Padakanti <102010729+SrikarPadakanti@users.noreply.github.com>
2025-03-11 10:44:09 +0530

    Create Bubble Sort.cpp

---
+++ Strivers A2Z DSA/Sorting/Bubble Sort.cpp
@@ -0,0 +1,41 @@
+Bubble Sort - GFG
+
+Given an array, arr[]. Sort the array using bubble sort algorithm.
+
+Examples :
+
+Input: arr[] = [4, 1, 3, 9, 7]
+Output: [1, 3, 4, 7, 9]
+Input: arr[] = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Input: arr[] = [1, 2, 3, 4, 5]
+Output: [1, 2, 3, 4, 5]
+Explanation: An array that is already sorted should remain unchanged after applying bubble sort.
+Constraints:
+1 <= arr.size() <= 103
+1 <= arr[i] <= 103
+
+
+class Solution {
+  public:
+    // Function to sort the array using bubble sort algorithm.
+    void bubbleSort(vector<int>& arr) {
+        // Your code here
+        int n = arr.size();
+        
+        for(int i = n-1; i >= 0; i--){
+            int swap = 0;
+            for(int j = 0; j < i; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swap++;
+                }
+            }
+            if(swap == 0){
+                break;
+            }
+        }
+    }
+};
